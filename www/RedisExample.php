d01bffb0f35cec7958cef2d704b18a69cdccbf59
angelina <chumachenkoangelina@gmail.com>
2025-11-13 15:58:50 +0200

    Implement RedisExample class with SET, GET and KEYS operations

---
+++ www/RedisExample.php
@@ -0,0 +1,57 @@
+﻿<?php
+
+namespace App;
+
+use App\Helpers\ClientFactory;
+
+class RedisExample
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = ClientFactory::make('http://redis:6379/');
+    }
+
+    public function setValue($key, $value)
+    {
+        try {
+            $response = $this->client->post('', [
+                'form_params' => [
+                    'command' => "SET $key \"$value\""
+                ]
+            ]);
+            return $response->getBody()->getContents();
+        } catch (\Exception $e) {
+            return "Redis Error: " . $e->getMessage();
+        }
+    }
+
+    public function getValue($key)
+    {
+        try {
+            $response = $this->client->post('', [
+                'form_params' => [
+                    'command' => "GET $key"
+                ]
+            ]);
+            return $response->getBody()->getContents();
+        } catch (\Exception $e) {
+            return "Redis Error: " . $e->getMessage();
+        }
+    }
+
+    public function getAllKeys()
+    {
+        try {
+            $response = $this->client->post('', [
+                'form_params' => [
+                    'command' => "KEYS *"
+                ]
+            ]);
+            return $response->getBody()->getContents();
+        } catch (\Exception $e) {
+            return "Redis Error: " . $e->getMessage();
+        }
+    }
+}
